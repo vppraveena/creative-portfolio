5a491ae1f4a680850269f492cf54292afee0a9a6
Praveena <vppraveena@gmail.com>
2026-05-07 22:09:27 -0700

    updated website

--- app.js
+++ app.js
@@ -22,3 +22,21 @@ function goBack() {
 function openLink(url) {
   window.open(url, '_blank', 'noopener,noreferrer');
 }
+
+// Play YouTube video inline
+function playYouTube(el, videoId) {
+  var iframe = document.createElement('iframe');
+  iframe.src = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0';
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allowfullscreen', 'true');
+  iframe.setAttribute('allow', 'autoplay; encrypted-media');
+  iframe.style.position = 'absolute';
+  iframe.style.inset = '0';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.borderRadius = '12px';
+  el.innerHTML = '';
+  el.style.backgroundImage = 'none';
+  el.appendChild(iframe);
+  el.onclick = null;
+}
